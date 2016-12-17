0e8d80c8b1657ca94a5557de052e9ab151fac050
ymz <907200117@qq.com>
2016-12-17 21:38:04 +0800

    123

--- homework/baidu/src/js/index.js
+++ homework/baidu/src/js/index.js
@@ -2,5 +2,11 @@
  * Created by ymz on 16-12-14.
  */
 (function mou() {
-
+console.log(document.getElementsByClassName(".moreProduct"));
+document.getElementsByClassName("moreProduct")[0].onmouseenter = function(){
+    document.getElementsByTagName("ul")[0].style.display = "block";
+}
+document.getElementsByTagName("ul")[0].onmouseleave = function(){
+     this.style.display = "none";
+}
 })()
