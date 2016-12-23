b8663fd653e0619e2f6ef36ef1a081147e8c8e6b
ymz <907200117@qq.com>
2016-12-23 22:20:40 +0800

    123

---
+++ newyear/src/js/index.js
@@ -0,0 +1,11 @@
+/**
+ * Created by ymz on 16-12-23.
+ */
+(function () {
+/*    function switchImage() {
+        $('#luck').removeClass("luckone").addClass("lucktwo");
+    }*/
+setTimeout(function () {
+    $('#luck').removeClass("luckone").addClass("lucktwo");
+},1000)
+})()
