902ed3077afd80c1416f9ab2e78d9cd4cccef6b4
余明智 <ymz@yumingzhideMac-mini.local>
2016-12-24 16:12:08 +0800

    123

--- newyear/src/js/index.js
+++ newyear/src/js/index.js
@@ -5,7 +5,16 @@
 /*    function switchImage() {
         $('#luck').removeClass("luckone").addClass("lucktwo");
     }*/
-setTimeout(function () {
-    $('#luck').removeClass("luckone").addClass("lucktwo");
-},1000)
+    setTimeout(function () {
+        $('#luck').removeClass("luckone").addClass("lucktwo");
+    },1000);
+    setTimeout(function(){
+        $('#luck').removeClass("lucktwo").addClass("luckone");
+        $('#phone').addClass("phonemove");
+    },1500);
+    setTimeout(function(){
+        $('#red_right').addClass("red_rightmove");
+        $('#red_left').addClass("red_leftmove");
+        $('#star').addClass("starmove");
+    },2000);
 })()
