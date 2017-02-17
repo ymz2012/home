a116f29c2440c75316f038d89380efbe24883f95
yumingzhi <yumingzhi@dayima.com>
2017-02-17 18:52:50 +0800

    123

---
+++ homework/sevenweek/pubu/js/index.js
@@ -0,0 +1,63 @@
+/**
+ * Created by ymz on 17/2/17.
+ */
+
+
+$(document).ready(function(){
+    $(window).on("load",function(){
+        imgLocation();
+        //定义一个滚动
+        var dataImg = {"data":[{"src":"1.jpg"},{"src":"2.jpg"},{"src":"3.jpg"},{"src":"4.jpg"},{"src":"5.jpg"},{"src":"6.jpg"},{"src":"7.jpg"},{"src":"8.jpg"},{"src":"9.jpg"},{"src":"10.jpg"}]};
+        window.onscroll = function(){
+          if(scrollside()){
+              $.each(dataImg.data,function(index,value){
+                  var box = $("<div>").addClass("box").appendTo($("#container"));
+                  var content = $("<div>").addClass("content").appendTo(box);
+                  //console.log("../image/"+$(value).attr("src"));
+                  $("<img>").attr("src","./image/"+$(value).attr("src")).appendTo(content);
+              });
+              imgLocation();
+          }
+        };
+    });
+});
+
+function scrollside(){
+    var box = $(".box");
+    //获得最后一张照片的整体高度,就是到顶部的高度
+    var lastboxHeight = box.last().get(0).offsetTop+Math.floor(box.last().height()/2); //距离顶部的高度加上最后一张图片的一半
+    var documentHeight = $(document).width(); //当前的高度
+    var scrollHeight = $(window).scrollTop(); //鼠标滚动的高度
+
+    return (lastboxHeight<scrollHeight+documentHeight)?true:false;
+}
+
+
+
+function imgLocation(){
+    var box = $(".box");
+    var boxWidth = box.eq(0).width();  //获得图片宽度
+    var num = Math.floor($(window).width()/boxWidth);  //获得第一排图片数量
+    var boxArr = []; //一排中所有盒子的高度    //定义一个数组将第一排图片的宽度放进去
+    box.each(function(index,value){
+        /*console.log(index+'------'+value);*/
+        var boxHeight = box.eq(index).height();  //定义照片的高度
+        if(index<num){
+            boxArr[index] = boxHeight;
+            //console.log(boxArr);
+        }else{
+            var minboxHeight = Math.min.apply(null,boxArr);  //获得第一排最小高度的值
+            /*console.log(minboxHeight);*/
+            var minboxIndex = $.inArray(minboxHeight,boxArr); //获得第一排高度最下照片的下表
+            //console.log(minboxIndex);
+
+            $(value).css({  //定义下一排照片摆放的位置在第一排最小高度下面
+                "position":"absolute",
+                "top":minboxHeight,
+                "left":box.eq(minboxIndex).position().left
+            });
+
+            boxArr[minboxIndex]+=box.eq(index).height(); //重新计算最小高度,因为下一排在第一排最小高度下面摆放了一张之后高度增加了
+        }
+    })
+}
