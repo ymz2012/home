3d3dc4ad542a843fea65e3c788be7b23410e2013
yumingzhi <yumingzhi@dayima.com>
2017-02-22 17:21:03 +0800

    123

--- homework/sevenweek/jikecourse/src/js/index.js
+++ homework/sevenweek/jikecourse/src/js/index.js
@@ -2,8 +2,8 @@
  * Created by ymz on 17/2/4.
  */
 
-(function(){
-    //设置点击回到最上面按钮
+$(document).ready(function(){
+    //设置点击回到顶部按钮
     var screenh = document.documentElement.clientHeight || document.body.clientHeight;
     var oTop = document.getElementById("wrapper");
     var gototop = document.getElementById("gototop");
@@ -17,4 +17,33 @@
     gototop.onclick = function(){
         document.documentElement.scrollTop = document.body.scrollTop =0;
     }
-})()
+    //点击顶部搜索框特效
+
+    $('#search-btn').on("click",function(){
+        $('#searchbox').addClass('scale');
+    })
+    $('#close-btn').on("click",function(){
+        $('#searchbox').removeClass('scale');
+    })
+
+
+    //切换课程列表排版方式
+    $('.kuai-icon').on("click",function(){
+        $('#changeid').removeClass('lesson-list2').addClass('lesson-list');
+    })
+    $('.list-icon').on("click",function(){
+        $('#changeid').removeClass('lesson-list').addClass('lesson-list2');
+    })
+
+
+    //鼠标滑过块状排饭效果问题
+
+/*    $('.lesson-list li').each(function(index){
+        var linode = $(this);
+        $(this).mouseover(function(){
+
+        })
+    })*/
+})
+
+
