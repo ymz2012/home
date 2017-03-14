258b8e911f48771d209567660f4139ce37ed1d4f
ymz <907200117@qq.com>
2017-03-14 21:04:45 +0800

    123

---
+++ homework/11week/baidu/src/js/jike/index.js
@@ -0,0 +1,19 @@
+/**
+ * Created by ymz on 17/2/4.
+ */
+
+(function(){
+    var screenh = document.documentElement.clientHeight || document.body.clientHeight;
+    var oTop = document.getElementById("wrapper");
+    var gototop = document.getElementById("gototop");
+    oTop.style.top = screenh - oTop.offsetHeight + "px";
+    oTop_top = screenh - oTop.offsetHeight;
+    window.onscroll = function(){
+        var scrolltop = document.documentElement.scrollTop || document.body.scrollTop;
+        oTop.style.top = screenh - oTop.offsetHeight + scrolltop +"px";
+    }
+
+    gototop.onclick = function(){
+        document.documentElement.scrollTop = document.body.scrollTop =0;
+    }
+})()
