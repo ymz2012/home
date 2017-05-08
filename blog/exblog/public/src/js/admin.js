70b14ddf57dff344b31490cbb3554ad5037feb03
yumingzhi <yumingzhi@dayima.com>
2017-05-08 19:09:26 +0800

    123

--- blog/exblog/public/src/js/admin.js
+++ blog/exblog/public/src/js/admin.js
@@ -15,7 +15,7 @@
 	adminDetail.prototype.readDom = function() {
 		var _this = this;
 		//方法部分 ejs渲染
-		var readFun = {
+		 _this.readFun = {
 			ejsGet: function(option) {
 				var ohtml = new EJS({
 					'element': option.ejsid
@@ -35,16 +35,29 @@
 					url: '/adminBlog/getnews',
 					datatype: 'json',
 					success: function(data) {
-						console.log(data);
+
+						console.log({
+							data: data
+						});
+						//将数据库返回的时间数据中的T去掉
+						for(var i = 0; i < data.length; i++) {
+							data[i].newstime = data[i].newstime.split('T')[0];
+						}
+						_this.readFun.readGoodsDetail({
+							data: data
+						});
 					}
 				});
 			}
 		};
-		readFun.refreshNews();
+		
+		_this.readFun.refreshNews();
 	}
 	adminDetail.prototype.eventBind = function() {
+		var deleteId = null;
 		var _this = this;
 		var bindFun = {
+			//提交完成编辑按钮
 			btnsubmitClick: function(e) {
 				e.preventDefault();
 				//输入判断
@@ -80,12 +93,62 @@
 						success: function(data) {
 							console.log(data);
 							//refreshNews();
-							//window.location.reload();
+							window.location.reload();
 						}
-
 					});
+				}
+			},
+			//编辑修改单个文章按钮
+			editKnowClick: function() {
+				var updateId = null;
+				updateId = $(this).parent().prevAll().eq(3).html();
+				console.log(updateId);
+				$('#updateModal').modal('show');
+				$.ajax({
+					url: '/adminBlog/curnews',
+					type: 'get',
+					datatype: 'json',
+					data: {
+						item_id: updateId
+					},
+					success: function(data) {
+						console.log(data);
+						$('#unewstitle').val(data[0].newstitle);
+						$('#unewstype').val(data[0].newstype);
+						$('#unewscontent').val(data[0].newscontent);
+						var utime = data[0].newstime.split('T')[0];
+						$('#unewstime').val(utime);
+					}
+				})
+			},
+			//删除单个新闻按钮拿到item_id
+			deleteKnowClick: function() {
+				$('#deleteModal').modal('show');
+				deleteId = $(this).parent().prevAll().eq(3).html();
+				_this.deleteId = deleteId;
+			},
+			//点击确认删除这篇博文
+			confirmDeleteClick: function() {
+				if(deleteId) {
+					$.ajax({
+						url: '/adminBlog/delete',
+						type: 'post',
+						data: {
+							item_id: _this.deleteId
+						},
+						success: function() {
+							console.log('删除成功');
+							$('#deleteModal').modal('hide');
+							_this.readFun.refreshNews();
+						}
+					})
 				}
 			}
 		}
+
+		$('#btnsubmit').on('click', bindFun.btnsubmitClick);//点击新建
+		$('#panelDefault').on('click', '.btnEdit', bindFun.editKnowClick);//点击编辑
+		$('#panelDefault').on('click', '.btnDel', bindFun.deleteKnowClick);//点击删除
+		$('#confirmDelete').on('click', bindFun.confirmDeleteClick);//点击删除后点击确认
 	}
 })();
--- blog/exblog/routes/adminBlog.js
+++ blog/exblog/routes/adminBlog.js
@@ -3,33 +3,55 @@ var router = express.Router();
 var mysql = require('mysql');
 
 var connection = mysql.createPool({
-  host     : 'localhost',
-  port     : '3306',
-  user     : 'root',
-  password : '123456',
-  database : 'blog'
+	host: 'localhost',
+	port: '3306',
+	user: 'root',
+	password: '123456',
+	database: 'blog'
 });
 
 //用户新增文章
-router.post('/insert',function (req, res) {
-    var newstitle = req.body.newstitle,
-	    newstype = req.body.newstype,
-        newscontent = req.body.newscontent,
-        newstime = req.body.newstime;
-		connection.query('INSERT INTO `blog_table` (`newstitle`,`newstype`,`newscontent`,`newstime`) VALUES (?,?,?,?)',[newstitle,newstype,newscontent,newstime],function (error, result) {
-			console.log(result);
-            if(!error){
-                res.json('success');
-                console.log(result.insertId);
-            }
-       });
+router.post('/insert', function(req, res) {
+	var newstitle = req.body.newstitle,
+		newstype = req.body.newstype,
+		newscontent = req.body.newscontent,
+		newstime = req.body.newstime;
+	connection.query('INSERT INTO `blog_table` (`newstitle`,`newstype`,`newscontent`,`newstime`) VALUES (?,?,?,?)', [newstitle, newstype, newscontent, newstime], function(error, result) {
+		console.log(result);
+		if(!error) {
+			res.json('success');
+			console.log(result.insertId);
+		}
+	});
 });
 //获取新闻列表
 router.get('/getnews', function(req, res, next) {
-    connection.query('SELECT * FROM `blog_table` order by item_id desc',function (error, rows) {
-    		console.log(rows);
-        res.json(rows);
-    });
+	connection.query('SELECT * FROM `blog_table` WHERE `status` = 1 order by item_id desc', function(error, rows) {
+		console.log(rows);
+		res.json(rows);
+	});
+});
+
+//模态框取值
+router.get('/curnews', function(req, res) {
+	var item_id = req.query.item_id;
+	connection.query('SELECT * FROM `blog_table` WHERE `item_id` = ?', [item_id], function(error, rows) {
+		console.log(rows);
+		res.json(rows);
+	});
+});
+
+//删除博文的功能
+router.post('/delete', function(req, res) {
+	var item_id = req.body.item_id;
+	connection.query('UPDATE `blog_table` SET `status` = 2 WHERE `item_id` = ?', [item_id], function(error, result) {
+		if(!error) {
+			res.json('success');
+			console.log(result.affectedRows);
+		}
+		/*res.json(rows);*/
+	});
+
 });
 
 module.exports = router;
